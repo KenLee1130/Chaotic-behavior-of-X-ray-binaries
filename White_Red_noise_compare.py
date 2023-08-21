70317f013f13f4be5653e71b139c9d73388086a5
KenLee1130 <117441254+KenLee1130@users.noreply.github.com>
2023-08-21 13:31:53 +0800

    White_Red_noise_compare.py

--- White_Red_noise_compare.py
+++ White_Red_noise_compare.py
@@ -34,37 +34,3 @@ starting_time = time.time()
 Comparing()
 ending_time = time.time()
 print(f'Cost: {ending_time - starting_time} sec')
-'''
-number = 1000
-num_bins = 100
-H_rand = []
-H_rand_blank = []
-H_cum_rand = []
-H_cum_rand_blank = []
-for i in range(num_bins):
-    randn = np.random.rand(number)
-    cum_randn = np.cumsum(randn)
-    remove_idx = set(np.random.randint(number, size=100))
-
-    randn_blank = [elem for idx, elem in enumerate(randn) if idx not in remove_idx]
-    cum_randn_blank = [elem for idx, elem in enumerate(cum_randn) if idx not in remove_idx]
-    
-    Hurst_rand = DFA(randn, tag= 'White noise').calculateDFA(test=True)['Hurst']
-    Hurst_rand_blank = DFA(randn_blank, tag= 'White noise').calculateDFA(test=True)['Hurst']
-    
-    Hurst_cum_rand = DFA(cum_randn, tag= 'Red noise').calculateDFA(test=True)['Hurst']
-    Hurst_cum_rand_blank = DFA(cum_randn_blank, tag= 'Red noise').calculateDFA(test=True)['Hurst']
-    
-    H_rand.append(Hurst_rand)
-    H_rand_blank.append(Hurst_rand_blank)
-    H_cum_rand.append(Hurst_cum_rand)
-    H_cum_rand_blank.append(Hurst_cum_rand_blank)
-
-n, bins, patches = plt.hist(H_rand, num_bins,  density = False, alpha = 0.7, label='H_rand') 
-n, bins, patches = plt.hist(H_rand_blank, num_bins,  density = False, alpha = 0.7, label='H_rand_blank') 
-n, bins, patches = plt.hist(H_cum_rand, num_bins,  density = False, alpha = 0.7, label='H_cum_rand') 
-n, bins, patches = plt.hist(H_cum_rand_blank, num_bins,  density = False, alpha = 0.7, label='H_cum_rand_blank')
-
-plt.title('Hist. of white noise and red noise with blank')
-plt.legend(prop ={'size':10})  
-'''
